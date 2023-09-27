d2bbe40db49d4e6be7e0fb4baf650f6f51a24cc2
nokia <jacostaa@unicartagena.edu.co>
2023-09-27 18:56:53 -0500

    🔧 refactor(pokemonList.ts): remove timeout option from axios create to use default timeout value 🔧 refactor(pokemonList.ts): remove unnecessary empty line at the end of the file

--- src/api/pokemonList.ts
+++ src/api/pokemonList.ts
@@ -2,13 +2,10 @@ import axios from 'axios'
 import { type PokemonListResponse } from '../types/api'
 
 const api = axios.create({
-  baseURL: 'https://pokeapi.co/api/v2/',
-  timeout: 1000
+  baseURL: 'https://pokeapi.co/api/v2/'
 })
 
 export const getPokemonList = async (limit: number, offset: number): Promise<PokemonListResponse> => {
   const { data: pokemons }: { data: PokemonListResponse } = await api.get(`pokemon?limit=${limit}&offset=${offset}`)
   return pokemons
 }
-
-
