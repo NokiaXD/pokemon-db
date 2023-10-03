af85d36fe207ae915b4d6f290285a1d50be7a4fb
nokia <jacostaa@unicartagena.edu.co>
2023-10-02 19:01:22 -0500

    🔨 refactor(App.tsx): extract PokemonCard component to improve code modularity and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import usePokemonList from './hooks/usePokemonList'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import './App.css'
+import PokemonCard from './components/pokemonCards'
 
 function App (): JSX.Element {
   const { error, getData, pokemonList, next } = usePokemonList()
@@ -20,8 +21,9 @@ function App (): JSX.Element {
           </p>
         }
       >
-        <div className=" grid xl:grid-cols-5
-         sm:grid-cols-1
+        <div className="
+          grid xl:grid-cols-5
+          sm:grid-cols-1
           md:grid-cols-3
           gap-4">
           {
@@ -30,27 +32,10 @@ function App (): JSX.Element {
               .map((pokemon) => (
                 <div key={pokemon.name} className='m-auto'>
 
-                  <div className="max-w-[150px] min-w-[144px] rounded overflow-hidden shadow-lg">
-                    <div>
-                      <div className='relative'>
-                       {
-                        (Number(pokemon.id) < 10000) && <span className="absolute bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#{pokemon.id !== '' ? pokemon.id : 'missingno'}</span>
-                        }
-                        <img className="w-32 m-auto" src={pokemon.data.sprites?.other?.['official-artwork'].front_default ?? 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'} alt={pokemon.name} />
-                      </div>
-                      <div className="font-bold text-xl mb-2 text-center">{pokemon.name}</div>
-                      <div className="flex justify-center">
-                        {
-                          pokemon.data.types?.map((type) => (
-                            <span key={type.type.name} className="inline-block bg-gray-200 rounded-full px-2 text-sm font-semibold text-gray-700 mx-2 mb-1">{type.type.name}</span>
-                          ))
-                        }
-                      </div>
-                    </div>
-                  </div>
+                  <PokemonCard pokemon={pokemon}></PokemonCard>
+
                 </div>
               ))
-
           }
         </div>
       </InfiniteScroll>
